6ea31f6f67b3c32f9c0e02e65acc6c529db194d6
Jeffp <serialchiller09@gmail.com>
2026-09-02 15:30:19 -0500

    Play Store v51: bigger Deal and actions on phones

--- js/08-tutorial.js
+++ js/08-tutorial.js
@@ -7,7 +7,7 @@ function updateTutorialCountExplanation() {
   function inject(name) {
     if (document.querySelector('script[src*="' + name + '"]')) return;
     const s = document.createElement('script');
-    s.src = 'js/' + name + '?v=50';
+    s.src = 'js/' + name + '?v=51';
     document.head.appendChild(s);
   }
   function injectAll() {

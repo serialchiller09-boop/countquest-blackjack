12d263eac539f59306a270445293bc9d10296618
Jeffp <serialchiller09@gmail.com>
2026-09-04 15:39:20 -0500

    Table v62: bump overlay script cache to ?v=62

--- js/08-tutorial.js
+++ js/08-tutorial.js
@@ -7,7 +7,7 @@ function updateTutorialCountExplanation() {
   function inject(name) {
     if (document.querySelector('script[src*="' + name + '"]')) return;
     const s = document.createElement('script');
-    s.src = 'js/' + name + '?v=61';
+    s.src = 'js/' + name + '?v=62';
     document.head.appendChild(s);
   }
   function injectAll() {
